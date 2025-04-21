2cf9f0c194b756df5404a8258f01feed796977aa
yangk95 <khu.yang@my.metrostate.edu>
2025-04-21 18:47:02 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        //an X represents a bomb while the 0's represent a "open tile"
+        //an X represents a mine while the 0's represent a "open cell"
         Grid g = new Grid(16, 16);
 
         g.pickSquare(0, 0);
